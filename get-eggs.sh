2f65201abb26c43d0da8a97062364a885672a75e
Piero Proietti <piero.proietti@gmail.com>
2023-11-03 16:26:14 +0100

    bugfix ubuntu

--- get-eggs.sh
+++ get-eggs.sh
@@ -3,7 +3,11 @@
 source /etc/os-release
 
 # Read $ID
-if [ "$ID" = "debian" ] || [ "$ID_LIKE" = "debian" ] || [ "$ID_LIKE" = "ubuntu debian" ]; then
+if  [ "$ID" = "debian" ] || 
+    [ "$ID" = "ubuntu" ] || 
+    [ "$ID_LIKE" = "debian" ] || 
+    [ "$ID_LIKE" = "ubuntu" ] || 
+    [ "$ID_LIKE" = "ubuntu debian" ]; then
     source ./ppa.sh
 elif [ "$ID" = "arch" ] || [ "$ID_LIKE" = "arch" ]; then
     source ./aur.sh
