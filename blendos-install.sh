e1adae40ab1d8767cb037dcc32475ff4c951ab47
Piero Proietti <piero.proietti@gmail.com>
2023-11-26 20:19:41 +0100

    blendos testing

--- blendos-install.sh
+++ blendos-install.sh
@@ -1,4 +1,21 @@
+function is_breakfast {
+    if grep -Fxq "[is_breakfast]" /etc/pacman.conf; then
+        echo ">> blendos-install: breakfast already present!"
+        exit
+    fi    
+}
 
-echo "[breakfast]" >> /etc/pacman.conf
-echo "SigLevel = Never" >> /etc/pacman.conf
-echo "Server = https://pkg-repo.blendos.co" >> /etc/pacman.conf
+
+function main {
+   cp system.yaml /
+
+   is_breakfast
+
+   echo "[breakfast]" >> /etc/pacman.conf
+   echo "SigLevel = Never" >> /etc/pacman.conf
+   echo "Server = https://pkg-repo.blendos.co" >> /etc/pacman.conf
+   pacman -Sy akshara
+   akshara update
+}
+
+main
