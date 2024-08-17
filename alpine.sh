334ae901494742914f28072d7590524cb71c9861
Piero Proietti <piero.proietti@gmail.com>
2024-08-17 04:59:12 +0200

    util-linux

--- alpine.sh
+++ alpine.sh
@@ -17,6 +17,7 @@ function main {
     wget -O penguins-eggs-bash-completion-10.0.27-r1.apk https://sourceforge.net/projects/penguins-eggs/files/alpine/x86_64/penguins-eggs-bash-completion-10.0.27-r1.apk/download
     wget -O penguins-eggs-doc-10.0.27-r1.apk https://sourceforge.net/projects/penguins-eggs/files/alpine/x86_64/penguins-eggs-doc-10.0.27-r1.apk/download
     apk add penguins-eggs*
+    apk add utils-linux
     ln -s /usr/bin/doas /usr/bin/sudo
     echo "fuse" | tee /etc/modules-load.d/fuse.conf
 
