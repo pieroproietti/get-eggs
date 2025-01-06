330841eb8ef1555e52348c29adfd9d485ac9ba2b
Piero Proietti <piero.proietti@gmail.com>
2025-01-06 14:11:22 +0100

    tarballs to finish

--- tarballs.sh
+++ tarballs.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -e
 source /etc/os-release
+
+case $ID in 
+    almalinux)
+        echo ">> get-eggs: OK, is Almalinux"
+        ;;
+    fedora)
+        echo ">> get-eggs: OK, is Fedora"
+        ;;
+    nobara)
+        echo ">> get-eggs: OK, is Nobara"
+        ;;
+    opensuse)
+        echo ">> get-eggs: OK, is Opensuse"
+        ;;
+    rocky)
+        echo ">> get-eggs: OK, is Rocky"
+        ;;
+esac
