2c8686b746983ee24c8aa53882063e6093378f5d
artisan <artisan@minimal>
2025-05-29 11:07:14 +0200

    adapments

--- tarballs.sh
+++ tarballs.sh
@@ -7,26 +7,24 @@ case $ID in
         ;;
     fedora)
         FOUND=1
-        echo ">> get-eggs: OK, is Fedora"
         ;;
     nobara)
         FOUND=1
-        echo ">> get-eggs: OK, is Nobara"
         ;;
     opensuse)
         FOUND=1
-        echo ">> get-eggs: OK, is Opensuse"
         ;;
     rocky)
         FOUND=1
-        echo ">> get-eggs: OK, is Rocky"
         ;;
 esac
 
 if [ $FOUND==1 ]; then
     echo ">> get-eggs: OK, is $ID"
+    echo "To install penguins-eggs"
+    echo "download penguins-eggs_10.1.1-1-linux-x64.tar.gz from https://penguins-eggs/basket"
     echo "sudo tarballs/requirements/$ID/install.sh"
-    echo "tarballs/setup penguins-eggs_10.1.1-1-linux-x64.tar.gz"
+    echo "tarballs/setup path/to/penguins-eggs_10.1.1-1-linux-x64.tar.gz"
 fi
 
 
