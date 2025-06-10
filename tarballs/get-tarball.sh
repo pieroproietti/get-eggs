8e0c2fd45110379e18b13f1c0f598b67aaf12cba
Piero Proietti <piero.proietti@gmail.com>
2025-06-10 22:55:05 +0200

    get-tarballs

--- tarballs/get-tarball.sh
+++ tarballs/get-tarball.sh
@@ -7,9 +7,8 @@ fi
 VERSIONS=$1
 
 VERSION="10.1.1-10"
-TAR_NAME="penguins-eggs_$VERSIOM-linux-x64.tar.gz"
+TAR_NAME="penguins-eggs_$VERSION-linux-x64.tar.gz"
 DOWNLOAD_PAGE="https://sourceforge.net/projects/penguins-eggs/files/Packages/tarballs/"
-
 echo wget -q  -O "$TAR_NAME" "${DOWNLOAD_PAGE}/$TAR_NAME/download"
 
 
