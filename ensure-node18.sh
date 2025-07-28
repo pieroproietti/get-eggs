fade45b2cfe3a13f2ff232c34e495bf62cd29b03
Piero Proietti <piero.proietti@gmail.com>
2025-07-28 06:28:22 +0200

    refresh pacchetti

--- ensure-node18.sh
+++ ensure-node18.sh
@@ -5,6 +5,9 @@ ensure_node18() {
   local available_versions
   available_versions=$(apt-cache policy nodejs 2>/dev/null | grep 'Candidate:' | awk '{print $2}' | cut -d'.' -f1)
 
+  # refresh dei pacchetti
+  apt-get update
+  
   for version in $available_versions; do
     if [[ "$version" =~ ^[0-9]+$ ]] && [ "$version" -ge "$NODE_MAJOR_VERSION" ]; then
       echo "Available $version. No need to add nodesource repo."
