b9503d40fe9e55071214012ad01f8a83f5a50d46
Piero Proietti <piero.proietti@gmail.com>
2025-07-28 08:29:56 +0200

    uffa

--- ensure-node18.sh
+++ ensure-node18.sh
@@ -24,7 +24,7 @@ ensure_node18() {
   echo "We need tp add nodejs>18 via nodesource repo"
   press_a_key_to_continue
   CMD="curl -fsSL "https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION.x" | bash -"
-  if $CMD: then
+  if $(CMD); then
     echo "Error execututing $CMD"
     exit 1
   fi
