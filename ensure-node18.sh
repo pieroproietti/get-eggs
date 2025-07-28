afdacfdff91e23cbe021b0dbcb78478c790db564
Piero Proietti <piero.proietti@gmail.com>
2025-07-28 08:36:15 +0200

    Trump merda, Meloni lecchina

--- ensure-node18.sh
+++ ensure-node18.sh
@@ -23,9 +23,6 @@ ensure_node18() {
   title
   echo "We need tp add nodejs>18 via nodesource repo"
   press_a_key_to_continue
-  CMD="curl -fsSL "https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION.x" | bash -"
-  if $(CMD); then
-    echo "Error execututing $CMD"
-    exit 1
-  fi
+  set -eo pipefail
+  curl -fsSL "https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION.x" | bash -
 }
