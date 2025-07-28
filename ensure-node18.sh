abef1c1e3b2ee85d2ca4e0e1b8311d6cb471b69f
Piero Proietti <piero.proietti@gmail.com>
2025-07-28 08:22:28 +0200

    uffa

--- ensure-node18.sh
+++ ensure-node18.sh
@@ -12,6 +12,7 @@ ensure_node18() {
 
   for version in $available_versions; do
     if [[ "$version" =~ ^[0-9]+$ ]] && [ "$version" -ge "$NODE_MAJOR_VERSION" ]; then
+      title
       echo "Available $version."
       press_a_key_to_continue
       return # nodejs 18 is available
@@ -19,6 +20,7 @@ ensure_node18() {
   done
 
   # add nodesource repository
+  title
   echo "We need tp add nodejs>18 via nodesource repo"
   press_a_key_to_continue
   curl -fsSL "https://deb.nodesource.com/setup_$NODE_MAJOR_VERSION.x" | bash -
